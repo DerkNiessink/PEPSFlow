1cacd4fdaf2be9a5e766f3f42f5adc6d05a09722
DerkNiessink <derk@niessink.com>
2024-12-11 10:28:41 +0100

    Added figure

--- src/pepsflow/cli/data.py
+++ src/pepsflow/cli/data.py
@@ -108,7 +108,7 @@ def plot(ctx, folders, **kwargs):
         for file in kwargs["gradient"].split(","):
             reader = iPEPSReader(os.path.join("data", folder, file))
             losses = reader.losses()
-            plt.plot(range(len(losses)), losses, "v-", markersize=4, linewidth=0.5, label=rf'$\{file}$')
+            plt.plot(range(len(losses)), losses, "v-", markersize=4, linewidth=0.5, label=rf'${file}$')
         plt.ylim(-0.665, -0.65)
         plt.xlim(0, 21)
         plt.xticks(range(int(min(range(len(losses)))), int(max(range(len(losses)))) + 2))
@@ -125,7 +125,7 @@ def plot(ctx, folders, **kwargs):
         
     plt.tight_layout()
     plt.legend()
-    plt.savefig("figures/Heisenberg_D3_opt.png")
+    plt.savefig("figures/Heisenberg_chi20_opt.png")
     plt.show()
 
 
